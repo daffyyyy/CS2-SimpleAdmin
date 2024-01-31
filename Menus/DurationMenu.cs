a266f776a8f8c2f775f916c27bd8a8251fd805ef
Valentin Barat <valentin@madboxgames.io>
2024-01-31 21:45:42 +0100

    Duration in minutes

--- Menus/DurationMenu.cs
+++ Menus/DurationMenu.cs
@@ -8,11 +8,11 @@ namespace CS2_SimpleAdmin.Menus
 	{
 		public static Tuple<string, int>[] _durations = new[]
 		{
-			new Tuple<string, int>("1 minute", 60),
-			new Tuple<string, int>("5 minutes", 60 * 5),
-			new Tuple<string, int>("15 minutes", 60 * 15),
-			new Tuple<string, int>("1 hour", 60 * 60),
-			new Tuple<string, int>("1 day", 60 * 60 * 24),
+			new Tuple<string, int>("1 minute", 1),
+			new Tuple<string, int>("5 minutes", 5),
+			new Tuple<string, int>("15 minutes", 15),
+			new Tuple<string, int>("1 hour", 60),
+			new Tuple<string, int>("1 day", 60 * 24),
 			new Tuple<string, int>("Permanent", 0)
 		};
 
