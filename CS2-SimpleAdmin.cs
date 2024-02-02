0572ad7d32be4b38a49bae9d8e044add6cea244c
Dawid Bepierszcz <41084667+daffyyyy@users.noreply.github.com>
2024-02-02 22:02:09 +0100

    1.2.9a
    - Major changes
    - Fixed `css_respawn`
    - Added discord webhook
    - Refactoring database class

--- CS2-SimpleAdmin.cs
+++ CS2-SimpleAdmin.cs
@@ -15,12 +15,7 @@ using Dapper;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using MySqlConnector;
-<<<<<<< Updated upstream
-using System;
-using System.Runtime.InteropServices;
-=======
 using Newtonsoft.Json;
->>>>>>> Stashed changes
 using System.Collections.Concurrent;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -51,13 +46,6 @@ public partial class CS2_SimpleAdmin : BasePlugin, IPluginConfig<CS2_SimpleAdmin
 	public override string ModuleDescription => "Simple admin plugin for Counter-Strike 2 :)";
 	public override string ModuleAuthor => "daffyy";
 	public override string ModuleVersion => "1.2.9a";
-
-<<<<<<< Updated upstream
-	public static MemoryFunctionVoid<CBasePlayerController, CCSPlayerPawn, bool, bool> CBasePlayerController_SetPawnFunc = new(
-		RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "\x55\x48\x89\xE5\x41\x57\x41\x56\x41\x55\x41\x54\x49\x89\xFC\x53\x48\x89\xF3\x48\x81\xEC\xC8\x00\x00\x00" : "\\x44\\x88\\x4C\\x24\\x2A\\x55\\x57"
-	);
-=======
->>>>>>> Stashed changes
 
 	public CS2_SimpleAdminConfig Config { get; set; } = new();
 
