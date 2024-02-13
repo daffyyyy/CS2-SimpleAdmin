64803ebff2b4dae6d45846e69066753bc97a5d06
Valentin Barat <valentin@madboxgames.io>
2024-02-13 23:54:57 +0100

    Updated version to 1.3.2b

--- CS2-SimpleAdmin.cs
+++ CS2-SimpleAdmin.cs
@@ -39,7 +39,7 @@ public partial class CS2_SimpleAdmin : BasePlugin, IPluginConfig<CS2_SimpleAdmin
 	public override string ModuleName => "CS2-SimpleAdmin";
 	public override string ModuleDescription => "Simple admin plugin for Counter-Strike 2 :)";
 	public override string ModuleAuthor => "daffyy & Dliix66";
-	public override string ModuleVersion => "1.3.2a";
+	public override string ModuleVersion => "1.3.2b";
 
 	public CS2_SimpleAdminConfig Config { get; set; } = new();
 
