5d58465c74ca55e135454a97068f9ff07b32e566
Valentin Barat <valentin@madboxgames.io>
2024-03-01 02:12:10 +0100

    Working custom server commands

--- Config.cs
+++ Config.cs
@@ -12,6 +12,16 @@ namespace CS2_SimpleAdmin
 		public string DiscordPenaltyWebhook { get; set; } = "";
 	}
 
+	public class CustomServerCommandData
+	{
+		[JsonPropertyName("Flag")]
+		public string Flag { get; set; } = "@css/generic";
+		[JsonPropertyName("DisplayName")]
+		public string DisplayName { get; set; } = "";
+		[JsonPropertyName("Command")]
+		public string Command { get; set; } = "";
+	}
+
 	public class CS2_SimpleAdminConfig : BasePluginConfig
 	{
 		public override int Version { get; set; } = 6;
@@ -54,5 +64,8 @@ namespace CS2_SimpleAdmin
 
 		[JsonPropertyName("WorkshopMaps")]
 		public List<string> WorkshopMaps { get; set; } = new List<string>();
+		
+		[JsonPropertyName("CustomServerCommands")]
+		public List<CustomServerCommandData> CustomServerCommands { get; set; } = new List<CustomServerCommandData>();
 	}
 }
