a8c4c1f9fac290da7f6b5465b5ac1ca57c4c089e
Dawid Bepierszcz <41084667+daffyyyy@users.noreply.github.com>
2024-03-02 21:53:48 +0100

    - 1.3.4a
    - Resolved conflicts?

--- Config.cs
+++ Config.cs
@@ -24,7 +24,7 @@ namespace CS2_SimpleAdmin
 
 	public class CS2_SimpleAdminConfig : BasePluginConfig
 	{
-		public override int Version { get; set; } = 7;
+		[JsonPropertyName("ConfigVersion")] public override int Version { get; set; } = 7;
 
 		[JsonPropertyName("DatabaseHost")]
 		public string DatabaseHost { get; set; } = "";
@@ -64,7 +64,7 @@ namespace CS2_SimpleAdmin
 
 		[JsonPropertyName("WorkshopMaps")]
 		public List<string> WorkshopMaps { get; set; } = new List<string>();
-		
+
 		[JsonPropertyName("CustomServerCommands")]
 		public List<CustomServerCommandData> CustomServerCommands { get; set; } = new List<CustomServerCommandData>();
 	}
