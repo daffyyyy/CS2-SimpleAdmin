c71391bca4783af16f291434841568f71fa761f9
Dawid Bepierszcz <41084667+daffyyyy@users.noreply.github.com>
2024-04-26 09:15:08 +0200

    1.4.0b
    - Recompiled with css v215

--- CS2-SimpleAdmin.cs
+++ CS2-SimpleAdmin.cs
@@ -37,7 +37,7 @@ public partial class CS2_SimpleAdmin : BasePlugin, IPluginConfig<CS2_SimpleAdmin
 	public override string ModuleName => "CS2-SimpleAdmin";
 	public override string ModuleDescription => "Simple admin plugin for Counter-Strike 2 :)";
 	public override string ModuleAuthor => "daffyy & Dliix66";
-	public override string ModuleVersion => "1.3.9a";
+	public override string ModuleVersion => "1.4.0b";
 
 	public CS2_SimpleAdminConfig Config { get; set; } = new();
 
