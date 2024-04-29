731bc229d356270c5c2ca985ae1278152c544495
Dawid Bepierszcz <41084667+daffyyyy@users.noreply.github.com>
2024-04-29 10:56:49 +0200

    1.4.2b
    - Fixed migrations for mysql 5.x

--- CS2-SimpleAdmin.cs
+++ CS2-SimpleAdmin.cs
@@ -37,7 +37,7 @@ public partial class CS2_SimpleAdmin : BasePlugin, IPluginConfig<CS2_SimpleAdmin
 	public override string ModuleName => "CS2-SimpleAdmin" + (Helper.IsDebugBuild ? " (DEBUG)" : " (RELEASE)");
 	public override string ModuleDescription => "Simple admin plugin for Counter-Strike 2 :)";
 	public override string ModuleAuthor => "daffyy & Dliix66";
-	public override string ModuleVersion => "1.4.2a";
+	public override string ModuleVersion => "1.4.2b";
 
 	public CS2_SimpleAdminConfig Config { get; set; } = new();
 
