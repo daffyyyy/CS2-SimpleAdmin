2432f37baccbebb95dd3d527e7ca35beb871343a
Sachin <sachinfnatic@gmail.com>
2024-11-09 01:02:57 +0530

    add: sa_players_ips table to upgrade .sql

--- CS2-SimpleAdmin/Database/Migrations/011_CSSPanelSupport.sql
+++ CS2-SimpleAdmin/Database/Migrations/011_CSSPanelSupport.sql
@@ -1,5 +1,14 @@
 ALTER TABLE `sa_servers` ADD `rcon` varchar(64) NOT NULL AFTER `address`;
 
+CREATE TABLE IF NOT EXISTS `sa_players_ips` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `steamid` bigint(20) NOT NULL,
+                                  `address` varchar(64) NOT NULL,
+                                  `used_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                  PRIMARY KEY (`id`),
+                                  UNIQUE KEY `steamid` (`steamid`,`address`) 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 CREATE TABLE IF NOT EXISTS `sa_chatlogs` (
 						 `id` int(11) NOT NULL AUTO_INCREMENT,
 						 `serverId` varchar(64) NOT NULL,
