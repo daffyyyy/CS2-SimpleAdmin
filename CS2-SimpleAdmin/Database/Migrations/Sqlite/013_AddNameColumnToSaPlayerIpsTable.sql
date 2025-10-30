038641dbdf786ba6e3b804f7ddfd62573e3c3d0a
Dawid Bepierszcz <41084667+daffyyyy@users.noreply.github.com>
2025-10-30 18:17:47 +0100

    Comment out MySQL index migration and remove Sqlite optimization
    Commented out all index creation statements in the MySQL migration 016 for table and index optimization. Removed the corresponding Sqlite migration 016 entirely. Also replaced TRUNCATE TABLE with DELETE FROM in Sqlite migration 013 for sa_players_ips to improve compatibility.

--- CS2-SimpleAdmin/Database/Migrations/Sqlite/013_AddNameColumnToSaPlayerIpsTable.sql
+++ CS2-SimpleAdmin/Database/Migrations/Sqlite/013_AddNameColumnToSaPlayerIpsTable.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE `sa_players_ips`;
+DELETE FROM sa_players_ips;
 
 ALTER TABLE `sa_players_ips` ADD `name` VARCHAR(64) NULL DEFAULT NULL;
 CREATE INDEX IF NOT EXISTS `idx_sa_players_ips_used_at` ON `sa_players_ips` (`used_at` DESC);
